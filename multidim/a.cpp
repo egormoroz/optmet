4ee9b27eecbbb9fb3b9c175182ac6e7152068568
Egor Moroz <egormoroz2020@gmail.com>
2021-10-09 17:30:01 +0300

    я покушал

--- multidim/a.cpp
+++ multidim/a.cpp
@@ -38,10 +38,20 @@ public:
 
     int num_calls() const { return m_counter; }
 
+    double dbg_eval(Complex z) const { return m_poly.eval(z); }
+    Complex dbg_eval_grad(Complex z) const {
+        return {m_poly.eval_der_x(z), m_poly.eval_der_y(z)};
+    }
+
 private:
     Polynomial m_poly;
     int m_counter = 0;
 };
+
+void log_iter(int k, Complex z0, double r, int calls) {
+ //   printf("%4d) f(%.5f + %.5fi) = %12.5e;\t%d calls\n",
+ //       k, z0.real(), z0.imag(), r, calls);
+}
 
 bool try_advance(Function &f, Complex &p, double &z, Complex step) {
     auto pp = p + step;
@@ -60,6 +70,8 @@ Complex coord_descent(Function &f, Complex p, Complex step, double eps) {
     Complex dx(step.real(), 0.), dy(0., step.imag()), pp;
     double z = f.eval(p), zz;
 
+    int k = 0;
+
     while (true) {
         pp = p; zz = z;
         int flag = 0;
@@ -75,6 +87,8 @@ Complex coord_descent(Function &f, Complex p, Complex step, double eps) {
         }
         dy /= 1.1;
 
+        log_iter(k++, p, z, f.num_calls());
+
         if (old_dx < eps && old_dy < eps)
             break;
     } 
@@ -86,7 +100,7 @@ Complex grad_descent_div(Function &f, Complex z0, double alpha, double delta) {
     const double EPS = 0.1;
     const double L = 0.5;
 
-    const bool OVERJUMP_GUARD = false;
+    int k = 0;
 
     Complex grad = f.eval_grad(z0);
     double val = f.eval(z0);
@@ -98,17 +112,11 @@ Complex grad_descent_div(Function &f, Complex z0, double alpha, double delta) {
             continue;
         }
 
-        Complex new_grad = f.eval_grad(z);
-        if (OVERJUMP_GUARD) {
-            //если градиент ВНЕЗАПНО развернулся
-            if (muls(new_grad, grad) < 0) {
-                alpha *= L;
-                continue;
-            }
-        }
         z0 = z;
-        grad = new_grad;
+        grad = f.eval_grad(z);
         val = new_val;
+
+        log_iter(k++, z0, val, f.num_calls());
     }
 
     return z0;
@@ -118,6 +126,7 @@ Complex grad_descent_const(Function &f, Complex z, double alpha, double delta) {
     Complex grad = f.eval_grad(z);
     double val = f.eval(z);
     int counter = 0;
+    int k = 0;
     while (norm_sqr(grad) >= delta && counter < 10) {
         z = z - alpha*grad;
         auto new_val = f.eval(z);
@@ -126,6 +135,7 @@ Complex grad_descent_const(Function &f, Complex z, double alpha, double delta) {
         }
         val = new_val;
         grad = f.eval_grad(z);
+        log_iter(k++, z, val, f.num_calls());
     }
 
     return z;
@@ -134,10 +144,12 @@ Complex grad_descent_const(Function &f, Complex z, double alpha, double delta) {
 Complex grad_descent_seq(Function &f, Complex z, int k, double delta) {
     Complex grad = f.eval_grad(z);
     double val = f.eval(z);
+    int j = 0;
     while (norm_sqr(grad) >= delta) {
         z = z - (1.0/k++) * grad;
         val = f.eval(z);
         grad = f.eval_grad(z);
+        log_iter(j++, z, val, f.num_calls());
     }
 
     return z;
@@ -147,6 +159,7 @@ Complex grad_descent_fastest(Function &f, Complex z, double delta) {
     const double EPS = 1e-5;
     Complex grad = f.eval_grad(z);
     double val = f.eval(z);
+    int k = 0;
     while (norm_sqr(grad) >= delta) {
         double alpha = golden_ratio([&](double alpha) {
             return f.eval(z - alpha * grad);
@@ -154,20 +167,21 @@ Complex grad_descent_fastest(Function &f, Complex z, double delta) {
         z = z - alpha * grad;
         val = f.eval(z);
         grad = f.eval_grad(z);
+        log_iter(k++, z, val, f.num_calls());
     }
 
     return z;
 }
 
 template<typename Method, typename... Args>
-void test_method(const char *name, Method m, Args&& ...args) {
+void test_method(const char *name, Method m, Complex z0, Args&& ...args) {
     Function f;
     printf("%s:\n", name);
     for (int i = 0; i < 3; ++i) {
-        Complex z0, r;
+        Complex r;
         if (i < 2) {
-            z0 = m(f, args...);
-            r = f.exclude_root(z0);
+          z0 = m(f, z0, args...);
+          r = f.exclude_root(z0);
         } else {
             z0 = f.get_last_root();
             r = 0;
@@ -179,13 +193,14 @@ void test_method(const char *name, Method m, Args&& ...args) {
     printf("\n");
 }
 
+
 int main() {
     using namespace std::complex_literals;
     auto start = 10.0 + 10i;
-    test_method(uwu(coord_descent), 0, start, 1e-5);
-    test_method(uwu(grad_descent_div), start, 1, 1e-5);
-    test_method(uwu(grad_descent_const), start, 1e-4, 1e-6);
-    test_method(uwu(grad_descent_seq), start, 20000, 1e-5);
-    test_method(uwu(grad_descent_fastest), 10.0 + 7i, 1e-5);
+    test_method(uwu(coord_descent), 0, start, 1e-3);
+    test_method(uwu(grad_descent_div), start, 1, 1e-3);
+    test_method(uwu(grad_descent_const), start, 1e-4, 1e-3);
+    test_method(uwu(grad_descent_seq), start, 10000, 1e-3);
+    test_method(uwu(grad_descent_fastest), start, 1e-3);
 }
 
