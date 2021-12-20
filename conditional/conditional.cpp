1961d6a0aabf4ca133bcd46d21adf2ef94de7b9c
Egor Moroz <egormoroz2020@gmail.com>
2021-12-20 17:01:06 +0300

    added conditional minimization

---
+++ conditional/conditional.cpp
@@ -0,0 +1,202 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <cstdio>
+#include "func.hpp"
+#include <algorithm>
+using namespace std;
+
+double g(Complex z) {
+    const double R = 6;
+    double x = z.real(), y = z.imag();
+    return x * x + y * y - R * R;
+}
+
+Complex g_grad(Complex z) {
+    return z * 2.;
+}
+
+double my_cos(Complex a, Complex b) {
+    return muls(a, b) / (norm(a) * norm(b));
+}
+
+void exterior_grad(Function &f, Complex &z, double step, double min_step, double eps) {
+    double r = 1.0;
+    int k = 1;
+    auto phi = [&](Complex z) {
+        double t = max(0.0, g(z));
+        double val = f.eval(z), penalty = r * t * t;
+
+        auto fg = f.dbg_eval_grad(z), gg = g_grad(z);
+        double cos_val = my_cos(-fg, gg);
+        double angle = acos(cos_val) * 180 / M_PI;
+        printf("%d; %d; (%f, %f); %f; %f; %f; (%f, %f); (%f, %f); %f; %f\n", 
+            k, f.num_calls(), z.real(), z.imag(), val, g(z), val + penalty,
+            fg.real(), fg.imag(), gg.real(), gg.imag(), cos_val, angle);
+
+        return val + penalty;
+    };
+
+    while(true) {
+        auto fg = f.eval_grad(z), gg = g_grad(z);
+        auto grad = fg + 2 * max(0.0, g(z)) * gg;
+        grad /= norm(grad);
+        double st = step, alpha = 0, val = phi(z);
+        while (st > min_step) {
+            double beta = alpha + st, new_val = phi(z - beta * grad);
+            if (new_val > val) {
+                st /= 2;
+                continue;
+            }
+
+            alpha = beta;
+            val = new_val;
+        }
+        z -= alpha * grad;
+
+        gg = g_grad(z);
+        fg = f.dbg_eval_grad(z);
+        double cos_val = my_cos(-fg, gg);
+        double angle = acos(cos_val) * 180 / M_PI;
+        printf("*%d; %d; (%f, %f); %f; %f; %f; (%f, %f); (%f, %f); %f; %f\n", 
+            k, f.num_calls(), z.real(), z.imag(), f.dbg_eval(z), g(z), val,
+            fg.real(), fg.imag(), gg.real(), gg.imag(), cos_val, angle);
+
+        if (g(z) < eps && my_cos(-f.eval_grad(z), gg) > 0.9994)
+            break;
+
+        ++k;
+        r *= 2;
+        z -= gg / norm(gg) * step;
+    }
+}
+
+void interior_grad(Function &f, Complex &z, double step, double min_step) {
+    double t = 1.0;
+    int k = 1;
+    auto psi = [&](Complex z) {
+        double val = f.eval(z), penalty = -t / g(z);
+
+        Complex fg = f.dbg_eval_grad(z);
+        Complex gg = g_grad(z);
+        double cos_val = my_cos(-fg, gg);
+        double angle = acos(cos_val) * 180 / M_PI;
+        printf("%d; %d; (%f, %f); %f; %f; %f; (%f, %f); (%f, %f); %f; %f\n", 
+            k, f.num_calls(), z.real(), z.imag(), f.dbg_eval(z), g(z), val,
+            fg.real(), fg.imag(), gg.real(), gg.imag(), my_cos(fg, gg), angle);
+
+        return val + penalty;
+    };
+
+    while(true) {
+        auto fg = f.eval_grad(z), gg = g_grad(z);
+        auto grad = fg + t / (g(z)*g(z));
+        grad /= norm(grad);
+
+        double st = step, alpha = 0, val = psi(z);
+        while (st > min_step) {
+            double beta = alpha + st;
+            auto zz = z - beta * grad;
+            if (g(zz) > 0) {
+                st /= 2;
+                continue;
+            }
+            double new_val = psi(zz);
+            if (new_val > val) {
+                st /= 2;
+                continue;
+            }
+
+            alpha = beta;
+            val = new_val;
+        }
+
+        z -= alpha * grad;
+
+        gg = g_grad(z);
+        fg = f.dbg_eval_grad(z);
+        double cos_val = my_cos(-fg, gg);
+        double angle = acos(cos_val) * 180 / M_PI;
+        printf("*%d; %d; (%f, %f); %f; %f; %f; (%f, %f); (%f, %f); %f; %f\n", 
+            k, f.num_calls(), z.real(), z.imag(), f.dbg_eval(z), g(z), val,
+            fg.real(), fg.imag(), gg.real(), gg.imag(), cos_val, angle);
+
+        if (my_cos(-f.eval_grad(z), gg) > 0.9994)
+            break;
+
+        ++k;
+        t /= 2;
+        z -= gg / norm(gg) * step;
+    }
+}
+
+void surf_border(Function& f, Complex& z, double eps) {
+    const double EPS = 1e-5;
+    const double L = 0.5;
+    const double APPROACH_STEP = 1.;
+    int k = 1;
+
+    Complex fg = f.eval_grad(z);
+    while (abs(g(z)) > EPS) {
+        fg /= norm(fg);
+        if (g(z - APPROACH_STEP * fg) > EPS) {
+            z -= g(z) / muls(g_grad(z), fg) * fg;
+        } else {
+            z -= APPROACH_STEP * fg;
+        }
+        fg = f.eval_grad(z);
+        printf("%d; %d; (%f, %f); %f; %f; (%f, %f); -; -; -; -\n", 
+            k, f.num_calls(), z.real(), z.imag(), f.dbg_eval(z), g(z),
+            fg.real(), fg.imag());
+        ++k;
+    }
+
+    Complex gg = g_grad(z);
+    double cos = my_cos(fg, gg);
+    const double TARGET_COS = 0.9994;
+    while (cos < TARGET_COS) {
+
+        Complex tan = -fg - gg * muls(-fg, gg) / norm_sqr(gg);
+        tan /= norm(tan);
+        const double alpha = 0.01;
+        z += tan * alpha;
+
+        if (g(z) > EPS)
+            z -= g(z) / norm_sqr(gg) * gg;
+
+        fg = f.eval_grad(z);
+        gg = g_grad(z);
+        cos = abs(muls(-fg, gg) / (norm(fg) * norm(gg)));
+        ++k;
+
+        printf("%d; %d; (%f, %f); %f; %f; (%f, %f); (%f, %f); (%f, %f); %f; %f\n", 
+            k, f.num_calls(), z.real(), z.imag(), f.dbg_eval(z), g(z),
+            fg.real(), fg.imag(), gg.real(), gg.imag(), tan.real(), tan.imag(), cos,
+            acos(cos) * 180 / M_PI);
+    }
+}
+
+
+int main() {
+    Complex z(0.);
+    Function f({4, 5}, {4, 9}, {9, 4});
+
+//    freopen("exterior.csv", "w", stdout);
+    printf("Iteration; Calls; (x, y); f(x, y); g(x, y);"
+            "phi(x, y); grad_f; grad_g; cos; angle\n");
+    exterior_grad(f, z, 0.1, 1e-3, 1e-3);
+
+    f.reset();
+    z = 0.;
+//    freopen("interior.csv", "w", stdout);
+    printf("Iteration; Calls; (x, y); f(x, y); g(x, y);"
+           "psi(x, y); grad_f; grad_g; cos; angle\n");
+    interior_grad(f, z, 0.1, 1e-3);
+
+    f.reset();
+    z = 0.;
+//    freopen("surf_border.csv", "w", stdout);
+    printf("Iteration; Calls; (x, y); f(x, y); g(x, y); grad_f; grad_g;"
+        "alpha * tan; cos(-grad_f, grad_g); angle\n");
+    surf_border(f, z, 0.);
+}
+
---
+++ conditional/func.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <complex>
+#include <cmath>
+
+using Complex = std::complex<double>;
+
+
+template<size_t N>
+Complex polyval(const Complex (&p)[N], Complex z) {
+    Complex val = 0;
+    for (int i = 0; i < N; ++i) {
+        val = val * z + p[i];
+    }
+    return val;
+}
+
+double muls(Complex a, Complex b) {
+    return a.real() * b.real() + a.imag() * b.imag();
+}
+
+double norm_sqr(Complex a) {
+    return muls(a, a);
+}
+
+double norm(Complex a) {
+    return sqrt(norm_sqr(a));
+}
+
+class Function {
+    Complex m_poly[4], m_der[3];
+    int m_counter;
+
+    void recalc_der() {
+        for (int i = 0; i < 3; ++i)
+            m_der[i] = double(3 - i) * m_poly[i];
+    }
+
+public:
+    Function(Complex z1, Complex z2, Complex z3) {
+        m_counter = 0;
+        m_poly[0] = 1.0;
+        m_poly[1] = -(z1 + z2 + z3);
+        m_poly[2] = z1 * z2 + z1 * z3 + z2 * z3;
+        m_poly[3] = -z1 * z2 * z3;
+        recalc_der();
+    }
+
+    double eval(Complex z) {
+        m_counter++;
+        return dbg_eval(z);
+    }
+
+    Complex eval_grad(Complex z) {
+        m_counter += 2;
+        return dbg_eval_grad(z);
+    }
+
+    double dbg_eval(Complex z) const {
+        return norm_sqr(polyval(m_poly, z));
+    }
+
+    Complex dbg_eval_grad(Complex z) const {
+        return 2.0 * std::conj(polyval(m_der, z)) * polyval(m_poly, z);
+    }
+
+    void reset() { m_counter = 0; }
+    int num_calls() const { return m_counter; }
+
+    Complex divide(Complex z) {
+        Complex k = 0;
+        Complex new_coeffs[4];
+        new_coeffs[0] = 0;
+        for (int i = 0; i < 3; ++i) {
+            k = k * z + m_poly[i];
+            new_coeffs[i + 1] = k;
+        }
+        Complex remainder = k * z + m_poly[3];
+
+        for (int i = 0; i < 4; ++i)
+            m_poly[i] = new_coeffs[i];
+        recalc_der();
+        return remainder;
+    }
+
+    Complex get_last_root() const {
+        return -m_poly[3] / m_poly[2];
+    }
+};
+
